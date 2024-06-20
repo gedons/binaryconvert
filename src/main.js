4a3be2327fe07831b6d9d1b9ac10581277d47c9e
Nerdy Gedoni <gedoniani@gmail.com>
2024-06-20 11:48:37 +0100

    pwa service worker added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import router from './router'
 import App from './App.vue'
+import './registerServiceWorker'; 
 
 import VueToast from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-sugar.css';
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Binary Decimal Converter',
+        short_name: 'Converter',
+        description: 'A simple binary and decimal converter.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icon.ico',
+            sizes: '192x192',
+            type: 'image/ico',
+          },
+          {
+            src: '/icon.ico',
+            sizes: '512x512',
+            type: 'image/ico',
+          },
+        ],
+      },
+    }),
+  ],
+});
